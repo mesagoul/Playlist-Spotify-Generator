e4afbcc2da311722e55050ecfb91986dc37fac96
BenKenobi <benjamin.padernoz@yahoo.fr>
2018-03-06 14:10:16 +0100

    [ DESIGN ] - Resetting default connection button

--- app/src/main/java/android/workshop/dmii/playlistspotifygenerator/activities/DashboardActivity.java
+++ app/src/main/java/android/workshop/dmii/playlistspotifygenerator/activities/DashboardActivity.java
@@ -1,8 +1,25 @@
 package android.workshop.dmii.playlistspotifygenerator.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by benja on 21/02/2018.
  */
 
-public class DashboardActivity {
+public class DashboardActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
