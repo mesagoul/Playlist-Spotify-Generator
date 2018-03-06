5c870c491debe506efb0ad7b1bad0ad769d16b7d
BenKenobi <benjamin.padernoz@yahoo.fr>
2018-03-06 11:20:26 +0100

    [ DESIGN ] - Add splash screen with Zazou logo

---
+++ app/src/main/java/android/workshop/dmii/playlistspotifygenerator/activities/SplashScreen.java
@@ -0,0 +1,43 @@
+package android.workshop.dmii.playlistspotifygenerator.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.workshop.dmii.playlistspotifygenerator.R;
+
+/**
+ * Created by benja on 06/03/2018.
+ */
+
+public class SplashScreen extends Activity {
+
+    private static final int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
