787312164c7de371a9f91c88663830caddc7d7d8
BenKenobi <benjamin.padernoz@yahoo.fr>
2018-03-28 20:11:02 +0200

    [ GENERAL ] - Update drawable menu options

--- app/src/main/java/android/workshop/dmii/playlistspotifygenerator/activities/DashboardActivity.java
+++ app/src/main/java/android/workshop/dmii/playlistspotifygenerator/activities/DashboardActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.workshop.dmii.playlistspotifygenerator.R;
 
 /**
@@ -54,7 +55,7 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        /*if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -66,6 +67,12 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
 
         } else if (id == R.id.nav_send) {
 
+        }*/
+
+        if(id == R.id.create_playlist){
+            Toast.makeText(this, "create playslit", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.generate_playlist){
+            Toast.makeText(this, "generate playslit", Toast.LENGTH_SHORT).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
