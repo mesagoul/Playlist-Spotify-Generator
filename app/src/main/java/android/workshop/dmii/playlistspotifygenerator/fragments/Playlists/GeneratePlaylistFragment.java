d80cc3830554feaef4e47e120f4a8815c02442c1
BenKenobi <benjamin.padernoz@yahoo.fr>
2018-04-02 12:36:47 +0200

    [ Generate Playlist ] - adding dropdown

--- app/src/main/java/android/workshop/dmii/playlistspotifygenerator/fragments/Playlists/GeneratePlaylistFragment.java
+++ app/src/main/java/android/workshop/dmii/playlistspotifygenerator/fragments/Playlists/GeneratePlaylistFragment.java
@@ -6,19 +6,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.workshop.dmii.playlistspotifygenerator.R;
+import android.workshop.dmii.playlistspotifygenerator.activities.DashboardActivity;
 import android.workshop.dmii.playlistspotifygenerator.models.User;
+import android.workshop.dmii.playlistspotifygenerator.network.SpotifyApiWrapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.CategoriesPager;
+import kaaes.spotify.webapi.android.models.Pager;
+import retrofit.client.Response;
 
 public class GeneratePlaylistFragment extends Fragment {
 
     private User user;
+    private SpotifyService spotify;
+    private ArrayList<String> categories;
 
     public GeneratePlaylistFragment() {
         user = User.getInstance();
+        spotify = SpotifyApiWrapper.getInstance().getService();
     }
 
     @Nullable
@@ -33,7 +52,33 @@ public class GeneratePlaylistFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Spinner spinner = (Spinner) getView().findViewById(R.id.genre_list);
 
+        Map<String, Object> options = new HashMap<>();
+        options.put("limit", 50);
+
+        spotify.getCategories(options, new SpotifyCallback<CategoriesPager>() {
+            @Override
+            public void failure(SpotifyError spotifyError) {
+                Log.d("GENERATE_FRAG",spotifyError.toString());
+            }
+
+            @Override
+            public void success(CategoriesPager categoriesPager, Response response) {
+
+                categories = new ArrayList<String>();
+
+                for(int i =0; i<categoriesPager.categories.total ; i++){
+                    String currentCategory = (String) categoriesPager.categories.items.get(i).name;
+                    categories.add(currentCategory);
+                }
+
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(dataAdapter);
+            }
+
+        });
     }
 
     @Override
